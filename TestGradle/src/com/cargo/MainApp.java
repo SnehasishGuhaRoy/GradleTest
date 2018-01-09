adb2dad4f07522fc819f50c7937e664d7551ed09
snehasishguharoy <snehasishguharoy871@gmail.com>
2018-01-09 23:29:44 +0530

    Gradle Test Project commit

---
+++ TestGradle/src/com/cargo/MainApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.cargo;
+
+/**
+ * @author sony
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!!!!!!");
+
+	}
+
+}
